2893ed7cda966f9cfab5be798e4f915f03d917e4
Ahmed Samy <151397435+AhmedSamyMahmoud@users.noreply.github.com>
2025-04-20 11:21:47 +0200

    Feat: add first integration

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -9,6 +9,7 @@ import youtubeIcon from "../../assets/Icon/social/youtube.svg";
 
 function Home() {
   const auth = useAuth();
+  const userID = JSON.parse(localStorage.getItem("token") || "{}")?.userId;
   return (
     <div className="home-container">
       <div className="home-container-header">
@@ -25,7 +26,15 @@ function Home() {
           flexDirection: window.innerWidth < 900 ? "column" : "row",
         }}
       >
-        <button className="social-option">
+        <button
+          className="social-option"
+          onClick={() => {
+            window.open(
+              `https://www.facebook.com/v19.0/dialog/oauth?client_id=${userID}&redirect_uri=https://api.mazeed.ai/api/oauth/instagram/callback&scope=instagram_basic,instagram_manage_insights,pages_show_list,pages_read_engagement&state=baf34b6e-411a-41f3-b4f7-29197f4f865v&response_type=code`,
+              "_blank"
+            );
+          }}
+        >
           <img
             src={facebookIcon}
             style={{ width: "24px", height: "24px", marginRight: "10px" }}
